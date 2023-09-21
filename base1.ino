48665989b2772a4b874aec2b0c33e02d0182ad48
fathel <125138378+fathelmarc@users.noreply.github.com>
2023-09-21 08:16:32 +0000

    Update base1.ino

--- base1.ino
+++ base1.ino
@@ -10,7 +10,7 @@ float toRad(float degree) {
   return degree * M_PI / 180;
 }
 
-float Keliling = 2 * 3.14 * R;
+float Keliling = 2 * M_PI * R;
 float w1, w2, w3, w4, fx, fy, v1, v2, v3, v4;
 
 void setup() {
@@ -20,18 +20,16 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  jarak(0,50);
-}
-void jarak (float target_x, float target_y) {
-  fx = target_x / Keliling * 360;
-  fy = target_y / Keliling * 360;
-  kinematic(fx, fy);
+  kinematic(100,0);delay(3000);
+  kinematic(0,100);delay(3000);
+  kinematic(-100,0);delay(3000);
+  kinematic(0,-100);
 }
 void kinematic(float x, float y) {
-  w1 = -sin(toRad( 45 )) * x + cos(toRad( 45 )) * y ;
-  w2 = -sin(toRad(135 )) * x + cos(toRad(135 )) * y ;
-  w3 = -sin(toRad(225 )) * x + cos(toRad(225 )) * y ;
-  w4 = -sin(toRad(315 )) * x + cos(toRad(315 )) * y ;
+  w1 = (-sin(toRad( 45 )) * x + cos(toRad( 45 )) * y )/Keliling;
+  w2 = (-sin(toRad(315 )) * x + cos(toRad(315 )) * y )/Keliling;
+  w3 = (-sin(toRad(225 )) * x + cos(toRad(225 )) * y )/Keliling;
+  w4 = (-sin(toRad(135 )) * x + cos(toRad(135 )) * y )/Keliling;
   Serial2.print('!'); Serial2.println(w1);
   Serial2.print('@'); Serial2.println(w2);
   Serial2.print('#'); Serial2.println(w3);

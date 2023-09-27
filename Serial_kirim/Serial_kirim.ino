d828209473e2413508a553b033fb5c129e4d807d
fathel <125138378+fathelmarc@users.noreply.github.com>
2023-09-27 08:14:23 +0000

    Update Serial_kirim.ino

--- Serial_kirim/Serial_kirim.ino
+++ Serial_kirim/Serial_kirim.ino
@@ -6,8 +6,10 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("#236");
-  delay(2000);
-  Serial.println("#1");
-  delay(2000);
+   Serial.print("$");Serial.println("1");
+  Serial3.print("$");Serial3.println("1");
+  delay(1000);
+  Serial3.print("$");Serial3.println("0");
+  Serial.print("$");Serial.println("0");
+  delay(1000);
 }
